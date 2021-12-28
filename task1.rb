abee74facb779bcc19d4af8e04e5078fa8cbf150
Vlad Fomenko <vlad.fomenko.ua@gmail.com>
2021-12-28 19:17:54 +0200

    task1 version 3 fixed some notes :color, :state in attr_reader rename Bricks_factory in BricksFactory initialization method changed, only one variable of unbroken bricks all method changed transfer of the create_state method to class Brick

--- task1.rb
+++ task1.rb
@@ -1,74 +1,59 @@
 class Brick
-  def initialize(color:, state:, number:)
+  attr_reader :color, :state
+  attr_accessor :number
+
+  def initialize(color, state, number = nil)
     @color = color
     @state = state
     @number = number
   end
 
-  attr_accessor :color, :state, :number
-
+  def self.create_state
+    result = rand(101)
+    result > 20 ? 'unbroken' : 'broken'
+  end
 
 end
 
-# module Arr_sort
-#   def sort_smth_field(field)
-#     @arr_bricks.filter {|i| i if i.state == 'unbroken'}.sort_by { |s| s.field}
-#   end
-#
-# end
-
-class Bricks_factory
-  def initialize(quantity)
-    @quantity = quantity
-    @arr_bricks = []
-    @temporary_var_state
-    @quantity_of_bricks = (1..@quantity).to_a
+class BricksFactory
+  def initialize
+    @arr_unbroken_bricks = []
   end
-
-  #include Arr_sort
-
-
-  def create_bricks
-    (1..@quantity).each {|i| @arr_bricks << Brick.new(color: create_color, state: create_state, number: create_number)}
-    @arr_bricks
-  end
-
 
   def create_color
-    %w[red white brown orange gray].shuffle.first
+    arr = %w[red white brown orange gray]
+    arr.sample
   end
 
-  def create_state
-    result = rand(101)
-    @temporary_var_state = result > 20 ? 'unbroken' : 'broken'
+  def create_number(value, number)
+    value == 'unbroken' ? number : nil
   end
 
-  def create_number
-    if @temporary_var_state == 'unbroken'
-      @quantity_of_bricks[0]
-      @quantity_of_bricks.shift
-    else
-      nil
+  def create_bricks(quantity)
+    number = 0
+    quantity.times do
+      temporary_object = Brick.new(create_color, Brick.create_state)
+      number += 1 if temporary_object.state == 'unbroken'
+      temporary_object.number = create_number(temporary_object.state, number)
+      @arr_unbroken_bricks << temporary_object
     end
+    @arr_unbroken_bricks
   end
 
   def only_unbroken_bricks
-    res = @arr_bricks.filter {|i| i if i.state == 'unbroken'}
+    res = @arr_unbroken_bricks.filter { |i| i if i.state == 'unbroken' }
     res.size
   end
 
   def bricks_some_color(user_color)
-    @result = []
-    @arr_bricks.reverse.filter do |i|
-      @result << i if i.color == user_color
-      break if @result.size > 9
-    end
-    @result.reverse
+    @arr_unbroken_bricks.reverse.each_with_object([]) do |element, result|
+      result << element if element.color == user_color
+      break result if result.size == 10
+    end.reverse
   end
 
   def sort_by_color
-    #не зміг уникнути повторення коду. Вже існує метод, який видаляє побиті цегли.
-    @arr_bricks.filter {|i| i if i.state == 'unbroken'}.sort_by { |s| s.color}
+    @arr_unbroken_bricks.sort_by { |s| s.color }
   end
 
 end
